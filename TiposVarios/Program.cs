30a0f24562a42314e0a42b8f732318ae5dbc9862
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 11:23:52 +0000

    TipoVarios corrections

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -15,28 +15,28 @@ namespace TiposVarios
             float floaty = 2.5f;
             double pi = 3.1415;
             long longBoi = 99999L;
-            decimal deximus = 6.969m;
-            uint uinter = 4;
+            decimal @decimal = 6.969m;
+            uint ui = 4;
 
             // Char variables
             char copyrightSymbol = '\u00A9';
             char zeroDash = '\u00D8';
 
             // Bool variables
-            bool alivent = false;
-            bool deadnt = true;
+            bool alivet = false;
+            bool dead = true;
 
             // Display the variables in the console
             Console.WriteLine(inter);
             Console.WriteLine(floaty);
             Console.WriteLine(pi);
             Console.WriteLine(longBoi);
-            Console.WriteLine(deximus);
-            Console.WriteLine(uinter);
+            Console.WriteLine(@decimal);
+            Console.WriteLine(ui);
             Console.WriteLine(copyrightSymbol);
             Console.WriteLine(zeroDash);
-            Console.WriteLine("Are you ok? " + alivent);
-            Console.WriteLine("Are you ok 2.0? " + deadnt);
+            Console.WriteLine("Are you ok? " + alive);
+            Console.WriteLine("Are you ok 2.0? " + dead);
         }
     }
 }
