65b78675a7d83b43a63cad40c3f84019e31f5db2
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 10:31:01 +0000

    Exercise 01 and 02

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    { /// <summary>
+      /// Code starts here.
+      /// </summary>
+      /// <param name="args"> Reads arguments </param>
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
