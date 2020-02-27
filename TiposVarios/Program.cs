74be6500547f312ec3f4574c9fb1af2ab313b22e
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 10:51:01 +0000

    Added unicode symbols, steps 5-7

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,7 @@ namespace TiposVarios
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // X
             int inter = 5;
             float floaty = 2.5f;
             double pi = 3.1415;
@@ -17,13 +18,20 @@ namespace TiposVarios
             decimal deximus = 6.969m;
             uint uinter = 4;
 
+            // X
+            char copyrightSymbol = '\u00A9';
+            char zeroDash = '\u00D8';
+
+
+            // X
             Console.WriteLine(inter);
             Console.WriteLine(floaty);
             Console.WriteLine(pi);
             Console.WriteLine(longBoi);
             Console.WriteLine(deximus);
             Console.WriteLine(uinter);
-
+            Console.WriteLine(copyrightSymbol);
+            Console.WriteLine(zeroDash);
         }
     }
 }
