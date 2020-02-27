7efac03a261a4ef2861db5eac35637d052dfed74
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 10:44:02 +0000

    Exercise 03 Steps 1-4

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        /// <summary>
+        /// Exercise revolving around different kinds of variables
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int inter = 5;
+            float floaty = 2.5f;
+            double pi = 3.1415;
+            long longBoi = 99999L;
+            decimal deximus = 6.969m;
+            uint uinter = 4;
+
+            Console.WriteLine(inter);
+            Console.WriteLine(floaty);
+            Console.WriteLine(pi);
+            Console.WriteLine(longBoi);
+            Console.WriteLine(deximus);
+            Console.WriteLine(uinter);
+
+        }
+    }
+}
