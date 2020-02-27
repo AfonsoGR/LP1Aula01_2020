927f9fdaaa5fe9918bb32de3d85da1f8f25ab582
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 12:00:57 +0000

    Ex05 Done, aka Ex04 Stage 02

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -8,18 +8,35 @@ namespace VariasStrings
         {
             // String variables
             string first = "A tab\t followed" 
-                + "by a new line\n followed by a unicode symbol \u03A9";
+            + "by a new line\n followed by a unicode symbol \u03A9";
             string second = @"Verbatim string, this does not add a tab \t";
             string third = @"This does not add a unicode symbol \u03A9 due 2 @";
             string fourth = "\"This string is between 'aspas'\"";
             string fifth = @"""This string is also in between 'aspas'""";
-            
+            string x = "onions";
+            string y = "shrek likes ";
+            string z = y + x;
+            string a = "bananas";
+            string b = "berries";
+            string c = $"eating {a} and {b} is very good"; 
+            string zc = $@"I'm an verbatim and interpoled string" 
+            + "as u can see ahead{z} {c}";
+            string alpha = String.Format("Arg {0} and {1}", x, y);
+            string beta = String.Format("Arg {0} and {1}", y, x);
+            string charlie = String.Format("Arg {1} and {0}", x, y);
+
             // Print the strings
             Console.WriteLine(first);
             Console.WriteLine(second);
             Console.WriteLine(third);
             Console.WriteLine(fourth);
             Console.WriteLine(fifth);
+            Console.WriteLine(z);
+            Console.WriteLine(c);
+            Console.WriteLine(zc);
+            Console.WriteLine(alpha);
+            Console.WriteLine(beta);
+            Console.WriteLine(charlie);
         }
     }
 }
