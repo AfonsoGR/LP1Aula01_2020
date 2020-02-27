819f74daf4357e357ddb087b64e524fb0fae3b9f
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-27 11:42:09 +0000

    Ex04 Stage 01 Complete

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,7 +6,20 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // String variables
+            string first = "A tab\t followed" 
+                + "by a new line\n followed by a unicode symbol \u03A9";
+            string second = @"Verbatim string, this does not add a tab \t";
+            string third = @"This does not add a unicode symbol \u03A9 due 2 @";
+            string fourth = "\"This string is between 'aspas'\"";
+            string fifth = @"""This string is also in between 'aspas'""";
+            
+            // Print the strings
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            Console.WriteLine(third);
+            Console.WriteLine(fourth);
+            Console.WriteLine(fifth);
         }
     }
 }
